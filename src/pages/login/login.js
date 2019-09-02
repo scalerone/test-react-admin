2b326dd1b2db17f5673c5bc48003e15d771fad75
sash <sash@nsfocus.com>
2019-09-02 16:54:44 +0800

    login静态登录页面

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,13 +1,44 @@
-import React, {Component} from 'react';
-
+import React, {Component} from 'react'
+import {
+    Form,
+    Input,
+    Icon,
+    Button,
+} from 'antd'
+import './login.less'
+import logo from '../../assets/images/logo.svg'
+const Item = Form.Item
+/*
+登陆路由组件
+*/
 class Login extends Component {
     render() {
         return (
-            <div>
-                login
+            <div className='login'>
+                <header className='login-header'>
+                    <img src={logo} alt="logo"/>
+                    <h1>React admin后台管理系统</h1>
+                </header>
+                <section className='login-content'>
+                    <h3>用户登陆</h3>
+                    <Form onSubmit={this.login} className="login-form">
+                        <Item>
+                            <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                   placeholder="用户名"/>
+                        </Item>
+                        <Item>
+                            <Input prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                   type="password" placeholder="密码"/>
+                        </Item>
+                        <Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                登录
+                            </Button>
+                        </Item>
+                    </Form>
+                </section>
             </div>
-        );
+        )
     }
 }
-
-export default Login;
+export default Login
