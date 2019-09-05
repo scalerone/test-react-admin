3532bf1bf002c011d4c20af8159a946ea78278b1
sash <sash@nsfocus.com>
2019-09-05 15:34:29 +0800

    使用store.js获取到登录用户的用户名在后台

--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
-
+import {Redirect} from 'react-router-dom'
+import memeoryUtils from '../../utils/memoryUtils';
 class Admin extends Component {
+
+
     render() {
+
+        const user = memeoryUtils.user
+        if(!user.id) {
+            return <Redirect to='/login'/>
+        }
         return (
             <div>
-                admin
+                <h2>后台管理</h2>
+                <div>Hello {user.name}</div>
             </div>
         );
     }
