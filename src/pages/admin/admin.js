2b32046e844e157555eada6563c67e00c5666f90
sash <sash@nsfocus.com>
2019-09-05 16:58:43 +0800

    后台基本框架布局

--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -1,20 +1,32 @@
 import React, {Component} from 'react';
 import {Redirect} from 'react-router-dom'
+import { Layout } from 'antd'
+
+
 import memeoryUtils from '../../utils/memoryUtils';
+import LeftNav from '../../components/left-nav'
+import Header from '../../components/header'
+
+const { Footer, Sider, Content } = Layout
 class Admin extends Component {
 
-
     render() {
-
         const user = memeoryUtils.user
         if(!user.id) {
             return <Redirect to='/login'/>
         }
         return (
-            <div>
-                <h2>后台管理</h2>
-                <div>Hello {user.name}</div>
-            </div>
+            <Layout style={{height: '100%'}}>
+                <Sider>
+                    <LeftNav/>
+                </Sider>
+                <Layout>
+                    <Header>Header</Header>
+                    <Content style={{backgroundColor: 'white'}}>Content</Content>
+                    <Footer style={{textAlign: 'center', color: '#aaaaaa'}}>推荐使用谷歌浏览器，
+                        可以获得更佳页面操作体验</Footer>
+                </Layout>
+            </Layout>
         );
     }
 }
