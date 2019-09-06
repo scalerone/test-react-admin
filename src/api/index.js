3e4f7e36ff808ece9a0be9e46112cdf3ef12f39b
sash <sash@nsfocus.com>
2019-09-06 18:57:50 +0800

    引入jsonp准备跨域

---
+++ src/api/index.js
@@ -0,0 +1,34 @@
+/*
+要求: 能根据接口文档定义接口请求
+包含应用中所有接口请求函数的模块
+每个函数的返回值都是promise
+
+基本要求: 能根据接口文档定义接口请求函数
+ */
+import jsonp from 'jsonp'
+import {message} from 'antd'
+
+/*
+json请求的接口请求函数
+ */
+export const reqWeather = (city) => {
+
+  return new Promise((resolve, reject) => {
+    const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+    // 发送jsonp请求
+    jsonp(url, {}, (err, data) => {
+      console.log('jsonp()', err, data)
+      // 如果成功了
+      if (!err && data.status==='success') {
+        // 取出需要的数据
+        const {dayPictureUrl, weather} = data.results[0].weather_data[0]
+        resolve({dayPictureUrl, weather})
+      } else {
+        // 如果失败了
+        message.error('获取天气信息失败!')
+      }
+
+    })
+  })
+}
+// reqWeather('成都')
